e8532d541ea992f276f9a1287b102dab99338350
passport90 <marhadiasak@gmail.com>
2016-12-10 01:09:51 +0700

    Fix Test duty_date Validation

--- spec/models/engineer_spec.rb
+++ spec/models/engineer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Engineer do
-  subject { build :engineer }
+  subject { build :engineer, duty_date: Faker::Date.between(Date.new(1970, 1, 1), Date.today) }
   it { should validate_presence_of(:slack_username) }
   it { should validate_uniqueness_of(:slack_username) }
   it { should validate_uniqueness_of(:duty_date) }
