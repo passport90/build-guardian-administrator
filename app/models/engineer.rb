0acb698ddf105346c681701f0b6619ce0f948dd3
passport90 <marhadiasak@gmail.com>
2016-12-09 08:56:16 +0700

    Allow nil in the Validation of duty_date Column

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,5 +1,5 @@
 class Engineer < ApplicationRecord
   validates :slack_username, presence: true, uniqueness: true
-  validates :duty_date, uniqueness: true
+  validates :duty_date, uniqueness: true, allow_nil: true
   validates :duty_fulfilled, exclusion: { in: [nil] }
 end
