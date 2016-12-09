efb38552b3da7f87371896e64560fa8439930c7f
passport90 <marhadiasak@gmail.com>
2016-12-09 08:57:10 +0700

    Controller Detect Current Hour
    Controller could differentiate between time and day

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,15 @@
 class MainController < ApplicationController
   def index
-    
+    if (7...19) === Time.now.hour
+      @time_of_day = "morning"
+      current_bg
+    else
+      @time_of_day = "night"
+    end
+  end
+
+private
+  def current_bg
+    @current_bg ||= Engineer.where(duty_date: Date.today).first
   end
 end
--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -1,9 +1,39 @@
 require 'rails_helper'
 
 describe "main page", :type => :feature do
-  it "displays Hello, world!" do
-    visit "/"
 
-    expect(page).to have_text("Hello, world!")
+  let!(:now) { Time.now }
+  context "morning" do
+    before do
+      allow(Time).to receive(:now) { now.change hour: Random.rand(7...19) }
+    end
+    it "displays Good morning" do
+      visit "/"
+
+      expect(page).to have_text("Good morning!")
+    end
+
+    # context "bg selected" do
+    #   it "shows the current BG" do
+    #     engineer = create :engineer, duty_date: Date.today
+    #     visit "/"
+    #
+    #     expect(page).to have_text("Welcome! The current BG is @#{engineer.slack_username}.")
+    #   end
+    # end
+  end
+
+  context "night" do
+    before do
+      allow(Time).to receive_message_chain(:zone, :now) do
+        now.change hour: Random.rand(19...31) % 24
+      end
+    end
+
+    it "displays Good night" do
+      visit "/"
+
+      expect(page).to have_text("Good night!")
+    end
   end
 end
