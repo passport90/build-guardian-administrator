2347a3fb510383ae32522a723870fc4f22728d8f
passport90 <marhadiasak@gmail.com>
2016-12-10 18:06:05 +0700

    Add Controller Tests

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -6,17 +6,19 @@ describe MainController, :type => :controller do
       get :index
       expect(response).to have_http_status(200)
     end
-    # 
-    # it "renders the index template" do
-    #   get :index
-    #   expect(response).to render_template("index")
-    # end
-    #
-    # it "loads all of the posts into @posts" do
-    #   post1, post2 = Post.create!, Post.create!
-    #   get :index
-    #
-    #   expect(assigns(:posts)).to match_array([post1, post2])
-    # end
+  end
+
+  describe "POST #select" do
+    it "responds successfully with an HTTP 302 status code" do
+      post :select
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "POST #conclude" do
+    it "responds successfully with an HTTP 302 status code" do
+      post :conclude
+      expect(response).to have_http_status(302)
+    end
   end
 end
